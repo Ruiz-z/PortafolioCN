5d091114417b12a327eee5c73bce12fe2f808dd3
Ruiz-z <mauroruiz0404@gmail.com>
2026-05-12 22:21:50 -0600

    feat: add Hero section with animated scroll indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App(){
   return(
     <div>
-      <Navbar/>
-      <h1>Portafolio de Evidencias CN</h1>
+      <Navbar />
+      <Hero />
     </div>
   )
 }

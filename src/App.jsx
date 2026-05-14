a00e83a9500ea363f02793185344a27ea21d3ad9
Ruiz-z <mauroruiz0404@gmail.com>
2026-05-13 22:51:16 -0600

    feat: add Evidencias section with unit grid and carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Indice from './components/Indice'
 import Introduccion from './components/Introduccion'
 import Objetivos from './components/Objetivos'
+import Evidencias from './components/Evidencias'
 
 function App(){
   return(
@@ -12,6 +13,7 @@ function App(){
       <Indice />
       <Introduccion />
       <Objetivos />
+      <Evidencias />
     </div>
   )
 }
